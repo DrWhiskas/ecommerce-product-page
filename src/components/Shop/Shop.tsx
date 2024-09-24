5a1a65159831b9fc6b4a72c7b79aee0d941e9f0b
unknown <kayv@hotmail.fr>
2024-09-24 21:51:31 +0200

    shop

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -20,20 +20,24 @@ export default function Shop({
 
 	return (
 		<div className="shop">
-			<div className="shop__number">{nb}</div>
 			<div className="shop__cart">
 				<h2 className="shop__cart__title">Cart</h2>
 				<div className="shop__cart__content">
 					<img
-						className="shop__cart__content"
+						className="shop__cart__content__img"
 						src={articleImg}
 						alt={articleName}
 					/>
+					<div className="shop__cart__content__info">
+						<p className="shop__cart__content__info__text">{articleName}</p>
+						<p className="shop__cart__content__info__price">
+							${articlePrice} x {nb} <span>${totalPrice}</span>
+						</p>
+					</div>
 				</div>
-				<div>
-					{' '}
-					{articlePrice} * {nb} = {totalPrice}{' '}
-				</div>
+			</div>
+			<div className="checkout">
+				<button className="shop__btn">Checkout</button>
 			</div>
 		</div>
 	);
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,15 +3,14 @@ import Header from '../../components/Header/Header';
 import Slider from '../../components/Slider/Slider';
 import Infos from '../../components/Infos/Infos';
 import './home.css'
-
+import IMG from '../../media/images/image-product-1-thumbnail.jpg';
 
 
 export default function Home() {
  
 	const [nb, setNb] = useState(0); // Gère l'état du nombre d'articles
 	const [total, setTotal] = useState(0)
- 	const articleImg = 'https://example.com/sneaker.jpg';
- 	const articleName = 'Limited Edition Sneaker';
+ 	const articleName = 'Fall Limited Edition Sneaker';
  	const articlePrice = 125;
 	function handleAddToCart(){		
 		setTotal(nb * articlePrice)	
@@ -21,7 +20,7 @@ export default function Home() {
 		<section className="home">
 			<Header
 				nb={nb}
-				articleImg={articleImg}
+				articleImg={IMG}
 				articleName={articleName}
 				articlePrice={articlePrice}
 				totalPrice={total}
